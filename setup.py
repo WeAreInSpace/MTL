0f18e2ba6862bf51d648b492d3d95683df1cdffe
WeAreInSpace <weareinspace.net@gmail.com>
2024-06-30 22:28:09 +0700

    modified:   .gitignore 	deleted:    MANIFEST.in 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 from setuptools import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 setup(
     name='MTL',
     version='1.0',
     description='A tool that help you to write HTML in Python',
-    long_description=readme(),
+    long_description='''<p style="text-align: center">\n<img src="./logo.png">\n</p>\n<h1 style="text-align: center">By We Are In Space</h1>\n<h3 style="text-align: center">Write HTML in Python with MTL</h3>\n\n## ⬇️ Installation\n\n**Use Pip to install packgages in your project.**\n\n```shell\npip install mtl\n```\n\n### 📚 Docs\n\n<a href="ff">https://www.weareinspace.net/mtl/docs</a>''',
     url='https://github.com/WeAreInSpace/MTL',
     author='We Are In Space',
     author_email='weareinspace.net@gmail.com',
     license='MIT',
-    install_requires=[
-        'matplotlib',
-        'numpy',
-    ],
     keywords='html in python website ssg',
     packages=['mtl'],
     package_dir={'mtl': 'src/mtl'},
