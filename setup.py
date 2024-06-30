c6dbb6762819586ceafb280aae1d51b3e76d7aef
WeAreInSpace <weareinspace.net@gmail.com>
2024-06-30 23:31:30 +0700

    modified:   .gitignore 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='MTL',
-    version='1.0',
+    name='mtlhtml',
+    version='1.1',
     description='A tool that help you to write HTML in Python',
-    long_description='''<p style="text-align: center">\n<img src="./logo.png">\n</p>\n<h1 style="text-align: center">By We Are In Space</h1>\n<h3 style="text-align: center">Write HTML in Python with MTL</h3>\n\n## ⬇️ Installation\n\n**Use Pip to install packgages in your project.**\n\n```shell\npip install mtl\n```\n\n### 📚 Docs\n\n<a href="ff">https://www.weareinspace.net/mtl/docs</a>''',
+    long_description='''### Write HTML in Python with MTL''',
+    long_description_content_type='text/markdown',
     url='https://github.com/WeAreInSpace/MTL',
-    author='We Are In Space',
+    author='WeAreInSpace',
     author_email='weareinspace.net@gmail.com',
     license='MIT',
     keywords='html in python website ssg',
     packages=['mtl'],
     package_dir={'mtl': 'src/mtl'},
-    package_data={'mtl': ['css/*.py']
-    },
+    package_data={'mtl': ['css/*.py']}
 )
